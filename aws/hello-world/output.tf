5dafae28750fea826aefb752337fbf605dbabf4d
samuel-br <62428459+samuel-br@users.noreply.github.com>
2022-11-24 16:59:42 +0200

    Update output.tf

--- aws/hello-world/output.tf
+++ aws/hello-world/output.tf
@@ -1,3 +1,9 @@
 output "url" {
   value = aws_s3_bucket.website_bucket.website_endpoint
 }
+output "masageeeeeeeeeeeeeeeeeee" {
+  value = 'aaaaaaasdfdsdadsfgffddwfdgfhgfsdasdsfdgfhgjkjyutyrterwdeasdsfdgfhtjyku  trtgerfegrethrytuyi  tjyrthdrt '
+}
+output "url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
+}
