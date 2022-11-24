907e8cb3cb60e5099fb323f97263d8ce709e31b7
samuel-br <62428459+samuel-br@users.noreply.github.com>
2022-11-24 17:28:17 +0200

    Update output.tf

--- aws/hello-world/output.tf
+++ aws/hello-world/output.tf
@@ -2,45 +2,11 @@ output "url" {
   value = aws_s3_bucket.website_bucket.website_endpoint
 }
 output "masageeeeeeeeeeeeeeeeeee" {
-  value = "Though yet of Hamlet our dear brother's death
-The memory be green, and that it us befitted
-To bear our hearts in grief and our whole kingdom
-To be contracted in one brow of woe,
-Yet so far hath discretion fought with nature
-That we with wisest sorrow think on him,
-Together with remembrance of ourselves.
-Therefore our sometime sister, now our queen,
-The imperial jointress to this warlike state,
-Have we, as 'twere with a defeated joy,--
-With an auspicious and a dropping eye,
-With mirth in funeral and with dirge in marriage,
-In equal scale weighing delight and dole,--
-Taken to wife: nor have we herein barr'd
-Your better wisdoms, which have freely gone
-With this affair along. For all, our thanks.
-Now follows, that you know, young Fortinbras,
-Holding a weak supposal of our worth,
-Or thinking by our late dear brother's death
-Our state to be disjoint and out of frame,
-Colleagued with the dream of his advantage,
-He hath not fail'd to pester us with message,
-Importing the surrender of those lands
-Lost by his father, with all bonds of law,
-To our most valiant brother. So much for him.
-Now for ourself and for this time of meeting:
-Thus much the business is: we have here writ
-To Norway, uncle of young Fortinbras,--
-Who, impotent and bed-rid, scarcely hears
-Of this his nephew's purpose,--to suppress
-His further gait herein; in that the levies,
-The lists and full proportions, are all made
-Out of his subject: and we here dispatch
-You, good Cornelius, and you, Voltimand,
-For bearers of this greeting to old Norway;
-Giving to you no further personal power
-To business with the king, more than the scope
-Of these delated articles allow.
-Farewell, and let your haste commend your duty."
+  value = "Though yet of Hamlet our dear brother's deathThe memory be green, and that it us befittedTo bear our hearts in grief and our whole kingdom
+To be contracted in one brow of woe,Yet so far hath discretion fought with natureThat we with wisest sorrow think on him,Together with remembrance of ourselves.Therefore our sometime sister, now our queen,The imperial jointress to this warlike state,
+Have we, as 'twere with a defeated joy,--\n
+With an auspicious and a dropping eye,\n
+With mirth in funeral and with dirge in marriage,In equal scale weighing delight and dole,--Taken to wife: nor have we herein barr'dYour better wisdoms, which have freely goneWith this affair along. For all, our thanks.Now follows, that you know, young Fortinbras,Holding a weak supposal of our worth,Or thinking by our late dear brother's deathOur state to be disjoint and out of frame,Colleagued with the dream of his advantage,He hath not fail'd to pester us with message,Importing the surrender of those landsLost by his father, with all bonds of law,To our most valiant brother. So much for him.Now for ourself and for this time of meeting:Thus much the business is: we have here writTo Norway, uncle of young Fortinbras,--Who, impotent and bed-rid, scarcely hearsOf this his nephew's purpose,--to suppressHis further gait herein; in that the levies,The lists and full proportions, are all madeOut of his subject: and we here dispatchYou, good Cornelius, and you, Voltimand,For bearers of this greeting to old Norway;Giving to you no further personal powerTo business with the king, more than the scopeOf these delated articles allow.Farewell, and let your haste commend your duty."
 }
 output "urllllll" {
   value = aws_s3_bucket.website_bucket.website_endpoint
